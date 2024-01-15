cedb0942f0d89d6f0f2f9cbe039a9ad5cb51d70a
apj7486 <apj7486@gmail.com>
2024-01-15 20:11:18 +0100

    module-4 done

---
+++ src/homework/3.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export function FormComponent() {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return <input type="text" value={value} onChange={handleChange} />;
+}
